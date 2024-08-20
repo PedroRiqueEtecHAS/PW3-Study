98584f72758faaa305c3ac5dc7d56933cb7a8ff0
Rique_Pedro <pedroriqueetec3@gmail.com>
2024-08-20 11:03:47 -0300

    primeiro commit

---
+++ PW3_Study/src/test/java/br/com/etechoracio/PW3_Study/Pw3StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.PW3_Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
