177a52d29ccd49e102526026967c44d59118125d
Henriqueluz21 <henrique.rique200611@gmail.com>
2024-09-01 18:10:29 -0300

    add Controllers, entify e repository

---
+++ PW3_Study/src/main/java/br/com/etechoracio/PW3_Study/controller/MonitorDisponibilidadeController.java
@@ -0,0 +1,4 @@
+package br.com.etechoracio.PW3_Study.controller;
+
+public class MonitorDisponibilidadeController {
+}
---
+++ PW3_Study/src/main/java/br/com/etechoracio/PW3_Study/repository/DisponibilidadeRepository.java
@@ -0,0 +1,4 @@
+package br.com.etechoracio.PW3_Study.repository;
+
+public interface DisponibilidadeRepository {
+}
