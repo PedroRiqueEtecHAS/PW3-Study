70f09d34118bf7041bc6f9d60e6e1837c51d14b1
Rique_Pedro <henrique.rique200611@gmail.com>
2024-09-10 11:04:32 -0300

    add Disciplinacontroller

--- PW3_Study/src/main/java/br/com/etechoracio/PW3_Study/controller/DisciplinaController.java
+++ PW3_Study/src/main/java/br/com/etechoracio/PW3_Study/controller/DisciplinaController.java
@@ -1,6 +1,8 @@
 package br.com.etechoracio.PW3_Study.controller;
+import br.com.etechoracio.PW3_Study.dto.DisciplinaResponseDTO;
 import br.com.etechoracio.PW3_Study.entify.Disciplina;
 import br.com.etechoracio.PW3_Study.repository.DisciplinaRepository;
+import br.com.etechoracio.PW3_Study.service.DisciplinaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,44 +17,11 @@ import java.util.List;
 public class DisciplinaController {
 
     @Autowired
-    private DisciplinaRepository repository;
+    private DisciplinaService service;
 
     @GetMapping
-    public List<Disciplina> listarDisciplinas() {return repository.findAll();}
+    public List<DisciplinaResponseDTO> listarDisciplinas() {return service.listar();}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Disciplina> buscarPorId(@PathVariable Long id){
-        var resposta = repository.findById(id);
-        if(resposta.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }else{
-            return ResponseEntity.ok(resposta.get());
-        }
-    }
-
-    @PostMapping
-    public ResponseEntity<Disciplina> inserir(@RequestBody Disciplina disciplina){
-        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(disciplina));
-    }
-    @PutMapping("/{id}")
-    public ResponseEntity<Disciplina> atualizar(@PathVariable Long id, @RequestBody Disciplina disciplina){
-        var existe = repository.findById(id);
-        if(!existe.isPresent())
-            return ResponseEntity.notFound().build();
-        return ResponseEntity.ok(repository.save(disciplina));
-
-    }
-    @DeleteMapping("{id}")
-    public ResponseEntity<Disciplina> excluir(@PathVariable Long id, @RequestBody Disciplina disciplina ){
-        var existe = repository.findById(id);
-        if(!existe.isPresent()){
-            return ResponseEntity.notFound().build();
-        }
-        else{
-            repository.deleteById(id);
-        }
-        return ResponseEntity.noContent().build();
-    }
 
 
 }
--- PW3_Study/src/main/java/br/com/etechoracio/PW3_Study/service/DisciplinaService.java
+++ PW3_Study/src/main/java/br/com/etechoracio/PW3_Study/service/DisciplinaService.java
@@ -1,14 +1,28 @@
 package br.com.etechoracio.PW3_Study.service;
 
+import br.com.etechoracio.PW3_Study.dto.DisciplinaResponseDTO;
+import br.com.etechoracio.PW3_Study.entify.Disciplina;
 import br.com.etechoracio.PW3_Study.repository.DisciplinaRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DisciplinaService {
 
     @Autowired
     private DisciplinaRepository repository;
+    private ModelMapper modelMapper = new ModelMapper();
+    public List<DisciplinaResponseDTO> listar() {
 
+        var disciplinas = repository.findAll();
+        var resultado = disciplinas.stream().map(
+                e -> modelMapper.map(e, DisciplinaResponseDTO.class)).collect(Collectors.toList());
+
+        return resultado;
+    }
 
 }
